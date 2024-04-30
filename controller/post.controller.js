36228bd3da2c12dde849ae715c4bfce050874ff9
Mukul <varun.gupta8851401313@gmail.com>
2024-04-30 18:35:52 +0530

    Created the functionality to create a Post

---
+++ controller/post.controller.js
@@ -0,0 +1,12 @@
+import Post from "../models/post.model.js"
+
+export const createPost = async ( request, response) => {
+    try{
+        const post = await new Post(request.body)
+        post.save()
+        return response.status(200).json({ msg: "Post saved successfully"})
+    }
+    catch ( error ){
+        return response.status(500).json({msg: error})
+    }
+}   
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,17 @@
 import express from "express"
 import { signUpUser, loginUser } from "../controller/user.controller.js"
-
+import { uploadImage, getimage } from "../controller/image.controller.js"
+import { createPost } from "../controller/post.controller.js"
+import { authenticateToken } from "../controller/jwt.controller.js"
+import upload from "../utils/upload.js"
 const router = express.Router()
 
 
 router.post("/signUp", signUpUser)
 router.post("/login", loginUser)
+router.post("/file/upload" , upload.single('file') ,uploadImage)
+router.post("/create" , authenticateToken ,createPost)
+router.get("/file/:filename", getimage )
+
 
 export default router;
