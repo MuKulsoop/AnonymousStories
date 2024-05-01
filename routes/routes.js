e96472c9ef075b14c98f5c1bf4e944c4cad81ade
Mukul <varun.gupta8851401313@gmail.com>
2024-05-01 14:22:42 +0530

    Completed the functionality of the Posts

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 import express from "express"
 import { signUpUser, loginUser } from "../controller/user.controller.js"
 import { uploadImage, getimage } from "../controller/image.controller.js"
-import { createPost } from "../controller/post.controller.js"
+import { createPost, getAllPosts, getPost } from "../controller/post.controller.js"
 import { authenticateToken } from "../controller/jwt.controller.js"
 import upload from "../utils/upload.js"
 const router = express.Router()
@@ -12,6 +12,8 @@ router.post("/login", loginUser)
 router.post("/file/upload" , upload.single('file') ,uploadImage)
 router.post("/create" , authenticateToken ,createPost)
 router.get("/file/:filename", getimage )
+router.get("/posts" , authenticateToken ,getAllPosts)
+router.get('/post/:id', authenticateToken, getPost)
 
 
 export default router;
