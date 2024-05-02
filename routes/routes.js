cd4abf5d37295c29dc5a99c8bd41c849c75e974f
Mukul <varun.gupta8851401313@gmail.com>
2024-05-02 21:38:58 +0530

    The project is completed but now I have to make it as I wish

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import express from "express"
 import { signUpUser, loginUser } from "../controller/user.controller.js"
 import { uploadImage, getimage } from "../controller/image.controller.js"
 import { createPost, getAllPosts, getPost, updatePost, deletePost } from "../controller/post.controller.js"
+import { newComment, getComments, deleteComment } from "../controller/comment.controller.js"
 import { authenticateToken } from "../controller/jwt.controller.js"
 import upload from "../utils/upload.js"
 const router = express.Router()
@@ -16,6 +17,8 @@ router.get("/posts" , authenticateToken ,getAllPosts)
 router.get('/post/:id', authenticateToken, getPost)
 router.put('/update/:id', authenticateToken, updatePost)
 router.delete('/delete/:id', authenticateToken, deletePost)
-
+router.post('/comment/new', authenticateToken, newComment)
+router.get('/comments/:id', authenticateToken, getComments)
+router.delete('/comment/delete/:id', authenticateToken, deleteComment)
 
 export default router;
